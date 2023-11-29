aa92c9283caa9b74452a7b0cc19dffac84b9ae04
natashasilveira <natashasilveira.p@gmail.com>
2023-11-29 00:08:06 -0300

    Inclusão de arquivos em assets e atualização da página home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import a4 from '../assets/a4.jpg'
+import heart from '../assets/heart.png'
 import styles from './Home.module.css'
 
 function Home() {
@@ -7,10 +7,11 @@ function Home() {
     <div className={styles.home}>
       <div className={styles.info_content}>
         <h1>Cicatrizes Invisíveis</h1>
-        <p>O projeto "Cicatrizes Invisíveis" é uma exposição artístico-informativa com intuito de expor e elucidar, através de textos e ilustrações, conceitos relacionados à práticas de violência psicológica contra a mulher em relações heterossexuais.</p>
+        <p>O projeto "Cicatrizes Invisíveis" é uma exposição artístico-informativa com intuito de expor e elucidar, através de textos e ilustrações, conceitos relacionados à 8 práticas de violência psicológica contra a mulher em relações heterossexuais.</p>
+        <p>Alerta de Gatilho de Conteúdo Sensível: Violência Psicológica Contra Mulher, incluindo representações de manipulação emocional, controle, isolamento e outras formas de abuso psicológico. Se você sentir que isso pode desencadear desconforto emocional ou lembranças dolorosas, recomendamos a cautela ao continuar.</p>
         <Link className={styles.btn} to='/exposition'>Ver Exposição</Link>
       </div>
-      <img src={a4} />
+      <img src={heart} />
     </div>
   )
 }
